30df7c14892918b314ec3fa4f9850934729db25f
Radu Toev <radu.toev@siemens.com>
2019-02-19 17:30:49 +0200

    Add some state to the mix

--- src/main/scala/Imp2Func.scala
+++ src/main/scala/Imp2Func.scala
@@ -7,24 +7,37 @@ object ImpCoinFlip extends App {
 
     val name = StdIn.readLine()
 
-    println(s"Hello, $name, welcome to the game")
+    println(s"Hello, $name, welcome to the game.")
 
-    var exec = true
+    var exec: Boolean = true
+    var correctGuesses: Int = 0
+    var totalGuesses: Int = 0
+
     while(exec) {
       val num = Random.nextInt(5) + 1
-      println(s"$name, please guess a number between 1 to 5:")
+      println(s"$name, please guess a number between 1 and 5:")
 
       val guess = StdIn.readLine().toInt
 
-      if(guess == num) println("You guessed right")
-      else println(s"You guessed wrong, the number was $num")
+      if(guess == num) {
+        correctGuesses += 1
+        println("You guessed right")
+      } else {
+        totalGuesses += 1
+        println(s"You guessed wrong, the number was $num")
+      }
 
       println("Do you want to continue?")
 
       StdIn.readLine() match {
-        case "y" => exec = true
-        case "n" => exec = false
-        case _   => exec = false
+        case "y" =>
+          exec = true
+        case "n" =>
+          exec = false
+          println(s"Thanks for playing. You had $correctGuesses guesses out of a total of $totalGuesses tries")
+        case _   =>
+          exec = false
+          println("Y u stupid?")
       }
     }
   }
